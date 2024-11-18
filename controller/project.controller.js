1b7d8c8e0c0fafb243ec369f1f5d79c57683a6a6
KILL-BURGER <dzaws@naver.com>
2024-11-19 00:34:28 +0900

    [feat] Add controller for main page API

--- controller/project.controller.js
+++ controller/project.controller.js
@@ -1,3 +1,18 @@
+const Project = require('../models/Project');
 const projectController = {};
+
+projectController.getAllProjects = async (req, res) => {
+  try {
+    // 검색
+    const {name} = req.query;
+    const cond = {isDeleted: false, ...(name ? {name: {$regex: name, $options: 'i'}} : {})};
+    let projectList = Project.find(cond);
+    let response = {status: 'success'};
+    response.data = await projectList.exec();
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(400).json({status: 'fail', error: error.message});
+  }
+};
 
 module.exports = projectController;
--- routes/project.api.js
+++ routes/project.api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const projectController = require('../controller/project.controller');
 
-
+router.get('/', projectController.getAllProjects);
 
 module.exports = router;
